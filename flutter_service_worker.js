1887e5f0c708bf54bdacc3fa9b66555511d97750
angloAyman <anglojhon4@gmail.com>
2025-09-29 15:21:25 +0300

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "49d38fbaa351fd924d2e4237216a8f73
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4c71c77655b437c3a1438fda1ac41a6b",
+"flutter_bootstrap.js": "6b842d2c05b803cb0461fbaac7ce8710",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
